03956bf27bba8cf4d02efc01572db4ef6f07c536
Тимурка Тимурка <xajrullin-02@mail.ru>
2022-02-03 17:14:49 +0300

    pjjjj

--- XamarinSysAdmin/Views/StorageListPage.xaml.cs
+++ XamarinSysAdmin/Views/StorageListPage.xaml.cs
@@ -36,43 +36,49 @@ namespace XamarinSysAdmin.Views
 
        async private void LViewPhoto_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            if (LViewPhoto.SelectedItem == null) return;
-
-            string result = await DisplayPromptAsync("_", "Введите число использованных материалов");
-            int i;
-            bool intParse = int.TryParse(result, out i);
-           if (!intParse)
+            try
             {
-                await DisplayAlert("Ошибка!","Введено некорректное число.Повторите попытку!","Ок");
-                SelectNull();
-                return;
-            }
-            
-            Storage storage = LViewPhoto.SelectedItem as Storage;
-            if (storage.Amount < i)
-            {
-                await DisplayAlert("Ошибка!", "На складе не хватает материалов!", $"Ок") ;
-                SelectNull();
-                return;
-            }
-            _materialList.AmountInList = i;
-            _materialList.ReqId = OrderSpecDetalPage.quire.IdQuire;
-            _materialList.MaterId = storage.IdMaterial;
+                if (LViewPhoto.SelectedItem == null) return;
 
-           if (Data.get().InsertMaterialList(_materialList))
-            {
+                string result = await DisplayPromptAsync("_", "Введите число использованных материалов");
+                int i;
+                bool intParse = int.TryParse(result, out i);
+                if (!intParse)
+                {
+                    await DisplayAlert("Ошибка!", "Введено некорректное число.Повторите попытку!", "Ок");
+                    SelectNull();
+                    return;
+                }
 
-                Data.get().UpdateStorage(storage, i);
-                await Shell.Current.GoToAsync("..");
-            }
-            else
-            {
-                await DisplayAlert("Ошибка!", "Возникла ошибка!", $"Ок");
-                SelectNull();
-                return;
-               
-            }
+                Storage storage = LViewPhoto.SelectedItem as Storage;
+                if (storage.Amount < i)
+                {
+                    await DisplayAlert("Ошибка!", "На складе не хватает материалов!", $"Ок");
+                    SelectNull();
+                    return;
+                }
+                _materialList.AmountInList = i;
+                _materialList.ReqId = OrderSpecDetalPage.quire.IdQuire;
+                _materialList.MaterId = storage.IdMaterial;
 
+                if (Data.get().InsertMaterialList(_materialList))
+                {
+
+                    Data.get().UpdateStorage(storage, i);
+                    await Shell.Current.GoToAsync("..");
+                }
+                else
+                {
+                    await DisplayAlert("Ошибка!", "Возникла ошибка!", $"Ок");
+                    SelectNull();
+                    return;
+
+                }
+            }
+            catch
+            {
+               await DisplayAlert("Ошибка", "Непредвиденная ошибка, повторите попытку!", "ok");
+            }
 
 
         }
